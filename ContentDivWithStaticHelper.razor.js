a2ef2060e0b613e586813c94c32f3f70b5c32ca9
Mayur Ekbote <mayur@fluidesk.com>
2022-08-19 00:01:49 +0530

    Add project files.

---
+++ ContentDivWithStaticHelper.razor.js
@@ -0,0 +1,43 @@
+﻿class ContentDivWithStaticHelper {
+
+    static dotNetHelper;
+    static dotNetHelperContainer = new Map();
+
+    static setDotNetHelper(value, id) {
+        ContentDivWithStaticHelper.dotNetHelper = value;
+    }
+
+    static async processKeyDown(keyEvent) {
+        keyEvent = keyEvent || window.event;
+        var caller = keyEvent.target || keyEvent.srcElement;
+        if (ContentDivWithStaticHelper.IsEnterType(keyEvent.key)) {
+            keyEvent.preventDefault();
+            await ContentDivWithStaticHelper.dotNetHelper.invokeMethodAsync('SaveContentRequested', caller.innerText);
+            caller.blur();
+        } else if (ContentDivWithStaticHelper.IsEscapeType(keyEvent.key)) {
+            await ContentDivWithStaticHelper.dotNetHelper.invokeMethodAsync('DiscardContentRequested');
+            caller.blur();
+        }
+    }
+
+    static async processBlur(event) {
+        await ContentDivWithStaticHelper.dotNetHelper.invokeMethodAsync('DiscardContentRequested');
+    }
+
+    static setPreviousContent(elem, content) {
+        elem.innerText = content;
+    }
+
+    static IsListNavigationType(key) {
+        return key === "ArrowDown" || key === "ArrowUp";
+    }
+    static IsEnterType(key) {
+        return key === "Enter";
+    }
+    static IsEscapeType(key) {
+        return key === "Escape";
+    }
+
+
+}
+window.ContentDivWithStaticHelper = ContentDivWithStaticHelper;
